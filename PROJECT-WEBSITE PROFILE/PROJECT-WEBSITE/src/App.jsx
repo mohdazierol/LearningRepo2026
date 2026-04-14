e9a757d0925a41fed90af321e727a310a2312d76
MOHD AZIEROL HAYAMIE BIN ANSAR <mohdazierol@gmail.com>
2026-04-14 21:09:44 +0800

    Prototype - Website Profile
    using free resource from tailwindcss . trying to recreate own personal prototype design .
    
    css all shamble need more time how to restructure the design

---
+++ PROJECT-WEBSITE PROFILE/PROJECT-WEBSITE/src/App.jsx
@@ -0,0 +1,39 @@
+import Part1 from "./components/part1.jsx";
+import Part2 from "./components/part2.jsx";
+import Part3 from "./components/part3.jsx";
+import Part4 from "./components/part4.jsx";
+import Part5 from "./components/part5.jsx";
+import Part6 from "./components/part6.jsx";
+import Part7 from "./components/part7.jsx";
+import Part8 from "./components/part8.jsx";
+import Part9 from "./components/part9.jsx";
+import Part10 from "./components/part10.jsx";
+import Part11 from "./components/part11.jsx";
+import Part12 from "./components/part12.jsx";
+import Part13 from "./components/part13.jsx";
+import Part14 from "./components/part14.jsx";
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={reactLogo} className="logo react" alt="React Logo" />
+      <Part1 />
+      <Part2 />
+      <Part3 />
+      <Part4 />
+      <Part5 />
+      <Part6 />
+      <Part7 />
+      <Part8 />
+      <Part9 />
+      <Part10 />
+      <Part11 />
+      <Part12 />
+      <Part13 />
+      <Part14 />
+    </div>
+  )
+}
+export default App
